2ef99c8dcfdf5fd1995b3f39775d9dad328fa8cd
shakilfar201 <shakilfarhan15@gmail.com>
2021-10-27 14:13:18 +0600

    server-commit

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const port = process.env.PORT || 1000;
 app.use(cors());
 app.use(express.json());
 
-// user : genius;
-// pass : vonk2RsJ76KMAKQ2;
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g3sna.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
